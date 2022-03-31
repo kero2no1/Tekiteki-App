40b1a53535d7709a1997c747fa012e002dff80aa
Kero.T <kerokero.2no1@gmail.com>
2022-03-31 21:58:17 +0900

    first view のHello Helloを表示

--- py-web/djangostudy/settings.py
+++ py-web/djangostudy/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'djangofirst.apps.DjangofirstConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
