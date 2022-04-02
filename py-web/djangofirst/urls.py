e801736cf683a22a0ac56f85452e9610f51e97f9
Kero.T <kerokero.2no1@gmail.com>
2022-04-02 22:00:56 +0900

    はじめてアプリ完成

--- py-web/djangofirst/urls.py
+++ py-web/djangofirst/urls.py
@@ -20,4 +20,11 @@ app_name = 'djangofirst'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('pref_quiz', views.pref_quiz, name='pref_quiz'),
+    path('pref_result', views.pref_result, name='pref_result'),
+    path('random_quiz', views.random_quiz, name='random_quiz'),
+    path('quiz_result', views.quiz_result, name='quiz_result'),
+    path('wiki', views.wiki, name='wiki'),
+    path('wiki_result', views.wiki_result, name='wiki_result'),
+    
 ]
